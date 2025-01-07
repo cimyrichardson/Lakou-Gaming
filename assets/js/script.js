74972c6a086c7e1265f89f74832f5c901fe587b2
cimyrichardson <richardsonclifordcimy@gmail.com>
2025-01-07 06:52:55 -0500

    ch

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,13 @@ const addEventOnElements = function (elements, eventType, callback) {
   }
 }
 
+document.addEventListener('contextmenu', event => event.preventDefault());
+document.addEventListener('keydown', event => {
+    if (event.ctrlKey && (event.key === 'u' || event.key === 'U' || event.key === 's' || event.key === 'S')) {
+        event.preventDefault();
+    }
+});
+
 
 
 /**
