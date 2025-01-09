a1969b2f18c324856e3cafaa732578b4d45afaff
cimyrichardson <richardsonclifordcimy@gmail.com>
2025-01-08 20:53:04 -0500

    js_Change

---
+++ assets/js/translations.js
@@ -0,0 +1,15 @@
+// translations.js
+const translations = {
+    en: {
+        about: "About Us",
+        contact: "Contact",
+    },
+    fr: {
+        about: "À propos de nous",
+        contact: "Contact",
+    },
+    cr: {
+        about: "Sobre nosotros",
+        contact: "Contacto",
+    }
+};
