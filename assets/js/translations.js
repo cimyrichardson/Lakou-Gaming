3d9c8c4a031849f4a61f8fc1be49f90b3c3c4e21
swag-ger3 <emmanueledely@gmail.com>
2025-01-16 07:57:43 -0800

    traduction

--- assets/js/translations.js
+++ assets/js/translations.js
@@ -16,6 +16,12 @@ const translations = {
         welcome: "Bienvenue Sur Lakou Gaming"
         space: " Un espace pour tous les joueurs qui veulent jouer, partager et grandir ensemble."
         mind: "Ce que nous avons en tête"
+        news: "Actualités et titres"
+        uptaded: "Restez au courant des dernières nouvelles, tendances et événements concernant les jeux. Explorez, apprenez et connectez-vous avec Lakou Gaming !"
+        Fire: "Free Fire"
+        date: "1 Février 2025"
+        Free: "Championnat de Free Fire"
+        
     },
     cr: {
     }
