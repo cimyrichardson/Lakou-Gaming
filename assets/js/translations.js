41c98708b9876c8d2e46d36d04344487f7bf9dea
swag-ger3 <emmanueledely@gmail.com>
2025-01-16 07:11:20 -0800

    traduction

--- assets/js/translations.js
+++ assets/js/translations.js
@@ -12,7 +12,10 @@ const translations = {
         oméga: "À propos de nous",
         delta: "contact",
         langue: "Langues",
-        team: "Équipes",
+        team: "Rejoignez notre équipe",
+        welcome: "Bienvenue Sur Lakou Gaming"
+        space: " Un espace pour tous les joueurs qui veulent jouer, partager et grandir ensemble."
+        mind: "Ce que nous avons en tête"
     },
     cr: {
     }
